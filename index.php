e0405704b812fcd300978a3acd0746d49ec7224a
Diky Andy <dikyandi@gmail.com>
2024-02-19 19:17:29 +0700

    menambahkan paragraph

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <title>Coba Git Remote 2</title>
 </head>
 <body>
-    
+    <h1>Hello World</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo non beatae cupiditate, quasi ullam porro laborum id temporibus voluptatum incidunt nulla quis eos dolorum unde, soluta magni illo aperiam illum!</p>
 </body>
 </html>
